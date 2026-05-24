d65016b0940a4582c9e6bef42b49c39f45ccc0f2
Sidharth <sidharthacker@gmail.com>
2026-05-24 22:09:18 +0530

    format file class

--- src/ATM/File.java
+++ src/ATM/File.java
@@ -1,8 +1,10 @@
 package ATM;
 import java.io.*;
+
 public class File {
 
     public void input(String account) {
+
         try (FileWriter writer = new FileWriter("test.txt")) {
             writer.write(account);
             System.out.println("file has been written");
@@ -10,15 +12,20 @@ public class File {
             System.out.println("Could not write file");
         }
     }
-    public static String output(int Account_no) {
+
+    public String output(int account_no) {
+
         try(BufferedReader reader=new BufferedReader(new FileReader("test.txt"))){
             System.out.println("That file exits");
             String line;
+
             for(int i=0;i<2;i++) {
                 line= reader.readLine();
                 String arr[]=line.split(" ");
+
                 int num = Integer.parseInt(arr[3]);
-                if(Account_no==num){
+
+                if(account_no==num){
                     System.out.println("number found");
                     return line;
                 }
