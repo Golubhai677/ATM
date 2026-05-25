510f478899f2df3a8721c9f412233275016549ac
Sidharth <sidharthacker@gmail.com>
2026-05-25 12:41:00 +0530

    fix CurrentUser loadData Bug

--- src/ATM/Atm.java
+++ src/ATM/Atm.java
@@ -22,13 +22,13 @@ public class Atm {
         System.out.println("Card no: " + cardno);
         if(cardno != 0){
             currentUser.account = new Account(bank.loadData(cardno));
-        }
 
-        System.out.println("currentUser Acno: " + currentUser.account.acNumber);
-        System.out.println("currentUser AcH: " + currentUser.account.acHolderName);
-        System.out.println("currentUser Ac bal: " + currentUser.account.balance);
-        System.out.println("currentUser Card: " + currentUser.account.cardNumber);
-        System.out.println("currentUser Pin: " + currentUser.account.pin);
+            System.out.println("currentUser Acno: " + currentUser.account.acNumber);
+            System.out.println("currentUser AcH: " + currentUser.account.acHolderName);
+            System.out.println("currentUser Ac bal: " + currentUser.account.balance);
+            System.out.println("currentUser Card: " + currentUser.account.cardNumber);
+            System.out.println("currentUser Pin: " + currentUser.account.pin);
+        }
 //        System.out.println("currentUser " + );
     }
 }
