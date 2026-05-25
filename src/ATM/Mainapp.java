a5d6be3b896b9a6bafcac9b5d753b676f9450795
Sidharth <sidharthacker@gmail.com>
2026-05-25 20:49:37 +0530

    test verified commit

--- src/ATM/Mainapp.java
+++ src/ATM/Mainapp.java
@@ -4,12 +4,12 @@ public class Mainapp {
 
     public static void main(String[] args){
 
-        Atm atm = new Atm();
-
-        atm.start();
-//        File file =new File();
-//        file.input("12345 golu 5000 500 1234");
-//        String line=file.output(12345);
-//        System.out.println(line);
+//        Atm atm = new Atm();
+//
+//        atm.start();
+        File file =new File();
+        file.input("12345 golu 5000 500 1234");
+        String line=file.output(12345);
+        System.out.println(line);
     }
 }
