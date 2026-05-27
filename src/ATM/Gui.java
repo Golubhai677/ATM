8eea1e58735ccfa8d25aa783b3a952e7b034c651
sahil prakash <rjrajsing654@gmail.com>
2026-05-27 10:07:14 +0530

    get pull

--- src/ATM/Gui.java
+++ src/ATM/Gui.java
@@ -1,8 +1,75 @@
 package ATM;
-
+import java.awt.*;
+import java.awt.event.*;
 import java.util.Scanner;
+import javax.swing.*;
+ class frame extends JFrame implements ActionListener{
+    JTextField Text;
+    JButton[] numButtons = new JButton[10];
+    String s0="",s1="",s2="";
+     frame()
+    {
+        setTitle("Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-public class Gui{
+        // Display field
+        Text = new JTextField(15);
+        Text.setEditable(false);
+        Text.setBackground(Color.WHITE);
+        Text.setHorizontalAlignment(JTextField.RIGHT);
+         JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4, 5, 5)); // 4x4 grid with spacing
+        panel.setBackground(Color.BLUE);
+        //button field
+        for(int i=0;i<=9;i++)
+        {
+            numButtons[i]=new JButton(String.valueOf(i));
+            numButtons[i].addActionListener(this);
+        }
+        //add field
+        panel.add(Text);
+        for(int i=0;i<=9;i++)
+        {
+            panel.add(numButtons[i]);
+        }
+        
+    }
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+
+        if (s.charAt(0) >= '0' && s.charAt(0) <= '9') {
+            if (!s1.equals("")) {
+                s2 += s;
+            } else {
+                s0 += s;
+            }
+            Text.setText(s0 + s1 + s2);
+        } else if (s.equals("C")) {
+            s0 = s1 = s2 = "";
+            Text.setText("");
+        } else if (s.equals("=")) {
+            double result = 0;
+            if (s1.equals("+"))
+                result = Double.parseDouble(s0) + Double.parseDouble(s2);
+            else if (s1.equals("-"))
+                result = Double.parseDouble(s0) - Double.parseDouble(s2);
+            else if (s1.equals("*"))
+                result = Double.parseDouble(s0) * Double.parseDouble(s2);
+            else if (s1.equals("/"))
+                result = Double.parseDouble(s0) / Double.parseDouble(s2);
+
+            Text.setText(String.valueOf(result));
+            s0 = String.valueOf(result);
+            s1 = s2 = "";
+        } else {
+            s1 = s;
+            Text.setText(s0 + s1 + s2);
+        }
+    }
+};
+public class Gui 
+{
     Scanner scanner = new Scanner(System.in);
 
     int registrationPage(){
